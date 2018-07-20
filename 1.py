4a89695dbfeb38fa1acc2d3c958269b2d4e300c9
satori-do <vladimir.reznichenko@onix-systems.com>
2018-07-20 17:53:53 +0300

    DEVOPS-00. Start python basic function

---
+++ 1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+print "Hello World!!!"
+
+# Function definiution is here
+def printme( str ):
+   "This prints a passed string into this function"
+   print str
+   return
+
+# Now you can call printme Function
+printme("I'm first call for user defined function!")
+printme("Again second call to the same function")
+printme("")
