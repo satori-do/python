41e3518f3e57a6fc8079f423a37091f9fdde6085
satori-do <vladimir.reznichenko@onix-systems.com>
2018-07-24 12:55:24 +0300

    DEVOPS-03. Fix error...

--- 1.py
+++ 1.py
@@ -11,4 +11,4 @@ def printme( str ):
 # Now you can call printme Function
 printme("I'm first call for user defined function!")
 printme("Again second call to the same function")
-printmey("")
+printme("")
