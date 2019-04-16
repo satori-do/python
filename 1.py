13e101e2f0b525afe5e166e8dd2157ade1b31581
satori-do <volodymyrreznich@gmail.com>
2019-04-16 13:43:48 +0300

    Comments cleaning.

--- 1.py
+++ 1.py
@@ -1,22 +1,12 @@
 #! /usr/bin/python
 
-# from urlparse import urlparse
 import urlparse
-# zzz = self._query("USE dashboard; SELECT value FROM config_variable WHERE name='HOSTNAME';")
-# a = 'https://192.168.34.5'.replace('.', '_')
-a = 'https://stg55.metricinsights.com'.replace('.', '_')
-#c = '(('https://192.168.33.19',),)'.replace('.', '_')
+
+a = 'https://somewhere.earningmoney.com'.replace('.', '_')
 b = urlparse.urlparse(a)
-# .replace(".", "_")
-# a.replace('.', '_')
-# string.replace(s, 'est', '')
-# a = 'https://stg55.metricinsights.com' -> stg55_metricinsight_com
-# a = ''
-# if a == '': a = 'socket.gethostname()'
-# self.db_exists('dashboard')
 print b.hostname
+
 if a != '':
   host_name_from_db = 'https://192.168.34.5'.replace('.', '_')
   replased_hostname = urlparse.urlparse(host_name_from_db)
   print replased_hostname.hostname
-# socket.gethostname() = replased_hostname.hostname
