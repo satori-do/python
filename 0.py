4d79109f72c78030ec59479da7e4b8fd228321b8
satori-do <volodymyrreznich@gmail.com>
2019-06-05 17:18:45 +0300

    Making order...

---
+++ 0.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print ("") # Just empty output
+
+#-----------------------------------------------------------------------------
+print ("\nHello World!!!")
+print ('Hello World!!!\n')
+
+print ('"Hello" World!!!')
+print ("'Hello' World!!!\n")
+
+print ("\"Hello\" World!!!") #;lghk\srh\
+print ("\"Hello\"\nWorld!!!\n")
+
+#-----------------------------------------------------------------------------
+# int
+number = 5
+
+# float
+fnumber = 5.7
+
+# string
+name = "Eternity"
+
+# bool
+status = True
+
+print ( "\n",number )
+
+print ("\nIt is a", number*2)
+
+#-----------------------------------------------------------------------------
