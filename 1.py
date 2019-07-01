edf867e48d659cda8ed84365dac1ba6ec2167cf9
satori-do <volodymyrreznich@gmail.com>
2019-07-01 16:52:31 +0300

    Going 2to3.

--- 1.py
+++ 1.py
@@ -4,9 +4,9 @@ import urllib.parse
 
 a = 'https://somewhere.earningmoney.com'.replace('.', '_')
 b = urllib.parse.urlparse(a)
-print(b.hostname)
+print((b.hostname))
 
 if a != '':
   host_name_from_db = 'https://192.168.34.5'.replace('.', '_')
   replased_hostname = urllib.parse.urlparse(host_name_from_db)
-  print(replased_hostname.hostname)
+  print((replased_hostname.hostname))
--- 2.py
+++ 2.py
@@ -4,10 +4,10 @@
 def changeme( mylist ):
    "This changes a passed list into this function"
    mylist = [1,2,3,4]; # This would assign new reference in mylist
-   print "Values inside the function: ", mylist
+   print("Values inside the function: ", mylist)
    return
 
 # Now you can call changeme function
 mylist = [10,20,30];
 changeme( mylist );
-print "Values outside the function: ", mylist
+print("Values outside the function: ", mylist)
